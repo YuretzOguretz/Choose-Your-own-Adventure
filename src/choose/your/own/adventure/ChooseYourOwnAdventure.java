e5f915c420491979aa241ffa4322a6362d310b06
YuretzOguretz <99295219+YuretzOguretz@users.noreply.github.com>
2022-04-19 17:52:22 -0400

    Last edits on the code
    Last part of code was added.
    Some edits to output (spelling, etc.)

--- src/choose/your/own/adventure/ChooseYourOwnAdventure.java
+++ src/choose/your/own/adventure/ChooseYourOwnAdventure.java
@@ -16,7 +16,7 @@ public class ChooseYourOwnAdventure {
      */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("You wake up in a creepy durty dark room. \nThe last thing you remember is how you passed out last night on Jake's party. \nYou check your phone to call for help but it's not there so you start looking around and exploring the environment. \nEverything you found is a flashlight and a piece of paper with latin writings on it.\n");
+        System.out.println("You wake up in a creepy dirty dark room. The last thing you remember is how you passed out last night at Jake's party. You check your phone to call for help but it's not there so you start looking around and exploring the environment. Everything you found is a flashlight and a piece of paper with Latin writings on it.\n");
         System.out.println("What do you do?\n");
         System.out.println("A: Try to read it out loud ");
         System.out.println("B: Keep exploring ");
@@ -26,7 +26,7 @@ public class ChooseYourOwnAdventure {
         d1 = d1.toUpperCase();
         System.out.println("");
         if (d1.equals("A")){
-            System.out.println("You hear a loud sound and ground shake a bit as if there was a small earthquake happening. \nThe ghost apears from nowhere and tries to attack you.\n");
+            System.out.println("You hear a loud sound and the ground shake a bit as if there was a small earthquake was happening. The ghost appears from nowhere and tries to attack you.\n");
         }
         else if (d1.equals("B")){
             System.out.println("You find a key under the carpet which opens the door to the next room. \n");
@@ -53,14 +53,13 @@ public class ChooseYourOwnAdventure {
         String d2 = scan.nextLine();
         d2 = d2.toUpperCase();
         if (d2.equals("A") && (d1.equals("A") || d1.equals("C"))){
-            System.out.println("You get possesed and you twist your neck to death. Try again. ");
+            System.out.println("You get possesed and you twist your neck to death.");
         }
         else if (d2.equals("B") && (d1.equals("A") || d1.equals("C"))){
-            System.out.println("You get possesed and you twist your neck to death. Tyr again. ");
+            System.out.println("You get possesed and you twist your neck to death.");
         }
         else if (d2.equals("C")&& (d1.equals("A") || d1.equals("C"))){
-            System.out.println("Your face is so ugly even the ghost got scarred away when it saw you.\n" + "You desided to explore some more ");
-            System.out.println("You find a key under the carpet which opens the door to the next room. ");
+            System.out.println("Your face is so ugly even the ghost got scarred away when it saw you. You decided to explore some more");
             System.out.println("You find a key under the carpet which opens the door to the next room. \n");
             System.out.println("D: Walk in ");
             System.out.println("E: Yell \"is anybody there\"");
@@ -75,7 +74,7 @@ public class ChooseYourOwnAdventure {
             System.out.println("There was no response ");
         }
         else if (d2.equals("F") && d1.equals("B")){
-            System.out.println("You see that there's no floor in the next room and can't see the the bottom. You also see the writings on the wall \" The way in in your head\" ");
+            System.out.println("You see that there's no floor in the next room and can't see the bottom. You also see the writings on the wall \" The way is in your head\" ");
         }
        }
         else if (d2.equals("D") && d1.equals("B")){
@@ -90,6 +89,36 @@ public class ChooseYourOwnAdventure {
         
         if (d2.equals("E")){
             System.out.println("There was no response ");
+            System.out.println("\nWhat do you do?\n");
+            System.out.println("A: Walk in");
+            System.out.println("B: Dirrect a flashlight in the next room.");
+            System.out.print("Enter Your Decision: ");
+            String d3 = scan.nextLine();
+            d3 = d3.toUpperCase();
+            if (d3.equals("A")){
+                System.out.println("It appears that there was no floor in that room. You fall and die.");
+            }
+            else if (d3.equals("B")){
+                System.out.println("You see that there's no floor in the next room and can't see the the bottom. You also see the writings on the wall \" The way is in your head\" ");
+            }
+        }
+        
+        System.out.println("What do you do?\n");
+        System.out.println("A: Close your eyes and walk in the room ");
+        System.out.println("B: Try to  parkure over ");
+        System.out.println("C: Explore the room more ");
+        System.out.print("Enter Your Decision: ");
+        String d4 = scan.nextLine();
+        d4 = d4.toUpperCase();
+        
+        if (d4.equals("A")){
+            System.out.println("You fall and die.");
+        }
+        else if (d4.equals("B")){
+            System.out.println("You fall and die.");
+        }
+        else if (d4.equals("C")){
+            System.out.println("You find an exit right behind the covers. You win yey!");
         }
     }
     
